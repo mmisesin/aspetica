f769644b6f03e07b2b5ae231f6fda177236ea004
artemmisesin@gmail.com <artemmisesin@artems-macbook-pro.local>
2017-01-20 21:29:28 +0200

    Fixing the algorithm, adding red numpad, replacing textfields with labels

--- Sparzel/ViewController.swift
+++ Sparzel/ViewController.swift
@@ -8,122 +8,150 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
+    //setting up the colors
+    var pressedButtonColor: UIColor = UIColor(red: 1, green: 0.94, blue: 0.93, alpha: 1)
+    var pressedButtonTextColor: UIColor = UIColor(red: 0.67, green: 0.22, blue: 0.13, alpha: 1)
+    var pressedButtonBorder: UIColor = UIColor(red: 1, green: 0.81, blue: 0.78, alpha: 1)
+    var mainTextColor: UIColor = UIColor(red: 0.25, green: 0.28, blue: 0.31, alpha: 1)
+    var mainTint: UIColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1)
+    var labelsBackground: UIColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.1)
     var deleteColor: UIColor = UIColor(hexString: "#FF5533")
     
-    var activeTextField = UITextField()
+    //instantiating subviews
+    @IBOutlet weak var xField: UILabel!
+    @IBOutlet weak var yField: UILabel!
+    @IBOutlet weak var wField: UILabel!
+    @IBOutlet weak var hField: UILabel!
+    var textfields: [UILabel] = []
+    @IBOutlet weak var deleteButton: CustomButton!
     
-    var previousActive = UITextField()
-
-    @IBOutlet weak var xField: UITextField!
-    
-    @IBOutlet weak var wField: UITextField!
-    
-    @IBOutlet weak var yField: UITextField!
-    
-    @IBOutlet weak var hField: UITextField!
-    
-    var secondTap = false
-    
+    //supporting variables
+    var activeTextField: UILabel?
+    var previousActive: UILabel?
+    var secondTapDone = false
     var isTyping = false
     
-    var textfields: [UITextField] = []
-    
-    @IBAction func touchDigit(_ sender: CustomButton, forEvent event: UIEvent) {
-        let digit = sender.titleLabel!.text!
-        if !secondTap || isTyping {
-            activeTextField.text = activeTextField.text! + digit
-        } else {
-            activeTextField.text = digit
-            isTyping = true
-        }
-        secondTap = false
-        var result = 0.0
-        if hField.text != "" && wField.text != "" && xField.text != "" && yField.text != "" {
-        switch activeTextField.tag {
-        case 1:
-            result = Double(hField!.text!)! / Double(yField!.text!)! * Double(activeTextField.text!)!
-            wField.text = String(forTailingZero(temp: result))
-        case 2:
-            result = Double(wField!.text!)! / Double(xField!.text!)! * Double(activeTextField.text!)!
-            hField.text = String(forTailingZero(temp: result))
-        case 3:
-            result = (Double(hField!.text!)! / Double(yField!.text!)!)
-            xField.text = String(forTailingZero(temp: Double(activeTextField.text!)! / result))
-        case 4:
-            result = (Double(wField!.text!)! / Double(xField!.text!)!)
-            yField.text = String(forTailingZero(temp: Double(activeTextField.text!)! / result))
-        default: break
-            }
-        }
-    }
-    
-    @IBAction func touchTextField(_ sender: UITextField) {
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to:nil, from:nil, for:nil)
-
-        activeTextField = sender
+    //tapping the textfields
+    func handleTap(recognizer: UITapGestureRecognizer) {
+        activeTextField = recognizer.view as? UILabel
+        
+        //if tapping specific field for the first time
         if activeTextField != previousActive {
-            secondTap = false
-        }
-        if !secondTap {
-            for view in textfields {
-                if view != sender {
-                    view.textColor = UIColor(red: 0.25, green: 0.28, blue: 0.31, alpha: 1)
-                    view.backgroundColor = UIColor.clear
-                    view.tintColor = UIColor.clear
-                } else {
-                    view.textColor = deleteColor
-                    view.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.1)
-                    view.tintColor = UIColor.clear
-                }
-            }
-        } else {
-            for view in textfields {
-                if view == sender {
-                    view.tintColor = deleteColor
-                    view.backgroundColor = UIColor.clear
-                } else {
-                    view.textColor = UIColor(red: 0.25, green: 0.28, blue: 0.31, alpha: 1)
-                    view.tintColor = UIColor.clear
-                    view.backgroundColor = UIColor.clear
-                }
-            }
-        }
-        if secondTap{
-            secondTap = false
+            secondTapDone = false
             isTyping = false
         } else {
-            secondTap = true
+            if secondTapDone {
+                secondTapDone = false
+            } else {
+                secondTapDone = true
+            }
         }
-        previousActive = sender
+        
+        //first tap state
+        if !secondTapDone {
+            for view in textfields {
+                if view != activeTextField {
+                    view.textColor = mainTextColor
+                    view.backgroundColor = UIColor.clear
+                } else {
+                    view.textColor = deleteColor
+                    view.backgroundColor = labelsBackground
+                }
+            }
+        } else {
+            
+        //second tap state
+            for view in textfields {
+                if view == activeTextField {
+                    view.backgroundColor = UIColor.clear
+                } else {
+                    view.textColor = mainTextColor
+                    view.backgroundColor = UIColor.clear
+                }
+            }
+        }
+        
+        previousActive = activeTextField
     }
     
-    @IBAction func deleteButton() {
-        if secondTap {
-            activeTextField.text = ""
-        } else {
-            if activeTextField.text != "" {
-                activeTextField.text?.remove(at: (activeTextField.text?.index(before: (activeTextField.text?.endIndex)!))!)
+    //button view, when finger is down on the screen
+    @IBAction func touchDownDigit(_ sender: CustomButton) {
+        sender.backgroundColor = pressedButtonColor
+        sender.setTitleColor(pressedButtonTextColor, for: .normal)
+        sender.borderColor = pressedButtonBorder
+    }
+    
+    //pressing the button on a numpad
+    @IBAction func touchDigit(_ sender: CustomButton, forEvent event: UIEvent) {
+        
+        if let tappedField = activeTextField {
+            
+            //button view, when finger is released
+            sender.backgroundColor = UIColor.white
+            sender.setTitleColor(mainTextColor, for: UIControlState.normal)
+            sender.borderColor = mainTint
+            
+            //adding a digit to the display
+            let digit = sender.titleLabel!.text!
+            if secondTapDone || isTyping {
+                tappedField.text = tappedField.text! + digit
+            } else {
+                tappedField.text = digit
+                isTyping = true
+            }
+            
+            var result = 0.0
+            if hField.text != "" && wField.text != "" && xField.text != "" && yField.text != "" {
+                switch tappedField.tag {
+                case 1:
+                    result = Double(hField!.text!)! / Double(yField!.text!)! * Double(tappedField.text!)!
+                    wField.text = String(forTailingZero(temp: result))
+                case 2:
+                    result = Double(wField!.text!)! / Double(xField!.text!)! * Double(tappedField.text!)!
+                    hField.text = String(forTailingZero(temp: result))
+                case 3:
+                    result = Double(wField!.text!)! / Double(xField!.text!)! * Double(yField!.text!)!
+                    hField.text = String(forTailingZero(temp: result))
+                case 4:
+                    result = (Double(hField!.text!)! / Double(yField!.text!)!) * Double(xField!.text!)!
+                    wField.text = String(forTailingZero(temp: result))
+                default: break
+                }
+            }
+        }
+    }
+    
+    @IBAction func deleteButtonTapped() {
+        deleteButton.backgroundColor = UIColor.white
+        if let tappedField = activeTextField {
+            if !secondTapDone {
+                tappedField.text = ""
+            } else {
+                if tappedField.text != "" {
+                    tappedField.text?.remove(at: (tappedField.text?.index(before: (tappedField.text?.endIndex)!))!)
+                }
             }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        textfields = [xField, yField, wField, hField]
         
+        textfields = [xField, yField, wField, hField]
+
+        //setting up textfields
         for view in textfields {
-            view.tintColor = UIColor.clear
+            view.isUserInteractionEnabled = true
+            let recognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+            recognizer.delegate = self
+            view.addGestureRecognizer(recognizer)
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    //taking care of zero
     func forTailingZero(temp: Double) -> String{
         return String(format: "%g", temp)
     }
